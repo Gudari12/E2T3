abb8e6a871bf87e70406f335d52c7ba98bf41e12
swaggamer117 <75172532+swaggamer117@users.noreply.github.com>
2025-02-07 13:05:00 +0100

    wrpper
    wrapper

--- erronka2/prog/src/erronka2/wrapper.java
+++ erronka2/prog/src/erronka2/wrapper.java
@@ -8,15 +8,27 @@ import java.util.Arrays;
 @XmlRootElement (name="langileak")
 public class wrapper {
    //@XmlElementWrapper (name="langileak")
-   @XmlElement(name = "langile")
-   private XML [] langile;
+   @XmlElement(name = "bulegari")
+   private XML [] bulegari;
    
    public wrapper (){
-      this.langile = new XML [0];
-   }
+	      this.bulegari = new XML [0];
+	      this.saltzaile = new XMLAN [0];
+	   }
    public void addSocio (XML s){
-      this.langile=Arrays.copyOf(this.langile,this.langile.length+1);
-      this.langile[this.langile.length-1]=s;
+      this.bulegari=Arrays.copyOf(this.bulegari,this.bulegari.length+1);
+      this.bulegari[this.bulegari.length-1]=s;
+   }
+   @XmlElement(name = "saltzaile")
+   
+   private XMLAN [] saltzaile;
+  
+   public void addSocio2 (XMLAN b){
+      this.saltzaile=Arrays.copyOf(this.saltzaile,this.saltzaile.length+1);
+      this.saltzaile[this.saltzaile.length-1]=b;
    }
    
 }
+
+	
+	
